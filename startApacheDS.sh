da6319a9dc65754c736bd8326a2d08612d006644
Michael Schapira <michael.schapira@gmail.com>
2020-07-07 16:06:58 -0400

    pvc.init

--- startApacheDS.sh
+++ startApacheDS.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if ![ -f /var/lib/apacheds-2.0.0.AM26/rhocp/pvc.init ]; then
+    touch /var/lib/apacheds-2.0.0.AM26/rhocp/pvc.init
     echo "Copying default instance to the volume mount point"
     cp -r /var/lib/apacheds-2.0.0.AM26/default/* /var/lib/apacheds-2.0.0.AM26/rhocp/
     echo "changing ownership to be owned by apacheds user and group"
