a4ecb556e11f035a7262e92c757c5d4191577fd5
Michael Schapira <michael.schapira@gmail.com>
2020-07-07 15:41:04 -0400

    Update startApacheDS.sh

--- startApacheDS.sh
+++ startApacheDS.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo "Copying default instance to the volume mount point"
-cp -r /var/lib/apacheds-2.0.0.AM26/default/* /var/lib/apacheds-2.0.0.AM26/rhocp/
-echo "changing ownership to be owned by apacheds user and group"
-chown -R apacheds:apacheds /var/lib/apacheds-2.0.0.AM26/rhocp 
+if ![ -f /var/lib/apacheds-2.0.0.AM26/rhocp/pvc.init ]; then
+    echo "Copying default instance to the volume mount point"
+    cp -r /var/lib/apacheds-2.0.0.AM26/default/* /var/lib/apacheds-2.0.0.AM26/rhocp/
+    echo "changing ownership to be owned by apacheds user and group"
+    chown -R apacheds:apacheds /var/lib/apacheds-2.0.0.AM26/rhocp 
+else
+    echo "Volume has been initialized - Doing nothing"
 
 echo "Starting instance rhocp"
 /opt/apacheds-2.0.0.AM26/bin/apacheds console rhocp
