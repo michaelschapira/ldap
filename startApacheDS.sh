dd7c0207f092282b211c1515cd8ea6ffc5c9eb92
Michael Schapira <michael.schapira@gmail.com>
2021-02-04 17:15:40 -0500

    change instance path

--- startApacheDS.sh
+++ startApacheDS.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-ADS_INST_PATH="/opt/apacheds/apacheds-2.0.0.AM26/instances/rhocp" 
+ADS_INST_PATH="/opt/apacheds/instances/rhocp" 
 
 if [ ! -f ${ADS_INST_PATH}/pvc.init ]; then
     touch ${ADS_INST_PATH}/pvc.init
     echo "Copying default instance to the volume mount point"
     cp -r ${ADS_INST_PATH}/../default/* ${ADS_INST_PATH}/
-    echo "changing ownership to be owned by apacheds user and group"
+    #echo "changing ownership to be owned by apacheds user and group"
     #chown -R apacheds:apacheds /var/lib/apacheds-2.0.0.AM26/rhocp 
 else
     echo "Volume has been initialized - Doing nothing"
